61616f1e6fb3e3040d19606d79ffd4d7b15c9ff8
Quinton Pang Yi Xuan <quinton.qpyx@gmail.com>
2022-10-29 17:21:46 +0800

    Fix banner

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,9 +28,11 @@ const changeButtonColor  = () =>{
 const isInViewport = function (ele) {
     const rect = ele.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        // rect.top >= 0 &&
+        rect.left >= 0 
+        &&
+        // rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        //  &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 };
